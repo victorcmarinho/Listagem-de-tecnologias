12e3ace94a8d0eec7b35df1ce400d420bf641c4a
Victor Marinho <victor.marinho@hipr.com.br>
2020-04-10 23:33:49 -0300

    Project Crud and likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } =  request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body; 
+  const index = repositories.findIndex(r => r.id === id);
+  if(index === -1)
+    return response.status(400).json(true);
+  const updateRepo = {...repositories[index], ...{title, url, techs}};
+  repositories.splice(index,1, updateRepo);
+  return response.json(updateRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(r => r.id === id);
+  if(index !== -1) {
+    repositories.splice(index, 1);
+    return response.status(204).json();
+  }
+  return response.status(400).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(r => r.id === id);
+  if(index === -1)
+    return response.status(400).json();
+  repositories[index].likes += 1; 
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
